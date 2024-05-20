54c228aff17bd4046c7ea2f3e9f23d4bb5558ecb
Bihan Lakshitha Siriwardana <137089511+BLsiriwardana@users.noreply.github.com>
2024-05-20 12:52:44 +0530

    Updated. bluetooth with servo motor

--- Bluetooth_module/Bluetooth_module.ino
+++ Bluetooth_module/Bluetooth_module.ino
@@ -1,10 +1,31 @@
+#include <Servo.h>
+Servo motor = Servo();
 void setup() {
   // put your setup code here, to run once:
   pinMode(11, OUTPUT);
-  Serial.begin();
+  pinMode(5, OUTPUT);
+  motor.attach(3);
+  Serial.begin(9600); // to start serial communication Serial.begin(9600 b/s);
+  // = - assign 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  int x = Serial.read();
 
+  if(x == '1'){
+    digitalWrite(5, LOW);
+    digitalWrite(11, HIGH);
+    delay(500);
+    motor.write(0);
+    delay(100);
+    }
+  
+  if(x == '2'){
+    digitalWrite(11, LOW);
+    digitalWrite(5, HIGH);
+    delay(500);
+    motor.write(90);
+    delay(100);
+    }
 }
